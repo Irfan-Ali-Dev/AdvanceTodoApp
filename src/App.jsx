71fa004dff4794705d7f90ad7512212eb6ef1cd7
Irfan-Ali-Dev <yousafbuilders.it@gmail.com>
2026-05-03 04:41:31 +0500

    Advance todo assingment

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import {React , useState} from 'react'
+import SubTask from './component/SubTask'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [input1, setInput1] = useState('')
+    const [tasks, setTasks] = useState([])
+    
+    const GetCatogry = (e) => {
+        setInput1(e.target.value)
+    }
+    function CreateTask () {
+        if(input1 !== ''){
+            setTasks([...tasks, input1])
+            setInput1('')
+        }else{
+            alert('Please enter a task category')
+        }
+    }
+    const TaskMap = tasks.map((task, index) => 
+        <div className='ticket'
+        style={{ border: '1px solid black', borderRadius:'20px', margin:'10px', padding:'10px'}}
+        key={index}>
+            <span className='sub-ticket' style={{display:'flex', flexwrap:'wrap', justifyContent:'space-between', padding:'0px'}}>
+                <h2 className='tic-heading' style={{ margin:'0px'}}>{task}</h2>
+                <button className='tic-delete-btn' onClick={() => deleteTask(index)}
+                    style={{border:'none', padding: '0px', background:'none'}}
+                    >❌</button>
+            </span>
+            <SubTask />
+        </div>
+    )
+
+    const deleteTask = (index)=>{
+        console.log("delete task " , index);
+        tasks.splice(index , 1)
+        setTasks([...tasks])
+        
+    }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className="todo-container">
+        <h1 className='mainHeading'>Advance Todo Application</h1>
+        <input className='mainInput' 
+        type="text"
+        placeholder="Enter task Category"
+        value={input1}
+        onChange={GetCatogry}
+        />
+        <button className='main-btn' onClick={CreateTask}>Create Task</button>
+        <span style={{display:'flex', flexWrap:'wrap'}}>{TaskMap}</span>
+            
 
-      <div className="ticks"></div>
+        
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    </div>
   )
 }
 
